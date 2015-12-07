4450f76c52cb4ded7b8b3fe98fa6e63b0c085dcd
Josh Jennings <josh.jennings.85@gmail.com>
2015-12-07 13:15:25 -0600

    Pulled JavaFX window back into main() and grouped into sub-methods for easier classification of instantiatied controls.

--- src/WindowMaker.java
+++ src/WindowMaker.java
@@ -9,9 +9,13 @@ import javafx.stage.Stage;
 import static java.lang.System.*;
 
 /**
- * Created by Josh on 12/2/2015.
+ * This primary (main) class instantiates a window for data entry and creation.
+ * @author Josh Jennings
  */
-public class WindowMaker {
+public class WindowMaker extends Application {
+
+	TreeItem<String> root;
+	Region spacer;
 
     public static void main(String[] args) {
         //out.println("TEST");
@@ -22,7 +26,129 @@ public class WindowMaker {
         //TODO: update the splash screen with loading status
 
         //TODO: move primary window to another class
-        PrimaryWindow primaryWindow = new PrimaryWindow(args);
+        //PrimaryWindow primaryWindow = new PrimaryWindow(args);
+	    //according to http://stackoverflow.com/questions/31173540/exception-in-thread-main-java-lang-runtimeexception-unable-to-construct-appli
+	    //   the launch() method must be in the main method. launch() does not return until window is closed. Maybe a new thread?
+	    launch(args);
+    }
+
+    @Override public void start(Stage primaryStage) {
+        out.println("Window creation beginning.");
+
+	    //define horizontal spacer that will grow with the window
+	    spacer = new Region();
+	    HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        //create topPane
+        Node topPane = topPane();
+
+        //create leftPane
+        Node leftPane = leftPane();
+
+	    //create primaryPane
+        //TODO: add main pane controls (TableView)
+
+	    //create bottomPane
+	    Node bottomPane = bottomPane();
+
+        //create BorderPane (root)
+        BorderPane rootPane = new BorderPane();
+        rootPane.setTop(topPane);
+        rootPane.setLeft(leftPane);
+        //TODO:rootPane.setCenter(primaryPane);
+        rootPane.setBottom(bottomPane);
+
+        //create scene
+        Scene scene = new Scene(rootPane);
+
+        //create stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Window Tester");
+        out.println("Showing window.");
+        primaryStage.show();
+        out.println("Window displayed.");
+    }
+
+	/**
+	 * This method will create the topPane of the root BorderPane layout.
+	 * @return Returns the Node that will be displayed in the topPane.
+	 */
+	private Node topPane() {
+		TextField salesPerson = new TextField();
+		salesPerson.setEditable(true);
+		salesPerson.setMaxWidth(400);
+		salesPerson.setMinWidth(100);
+		salesPerson.setPrefWidth(200);
+		salesPerson.setPromptText("Sales Engineer");
+
+		HBox topPane = new HBox(10, spacer, salesPerson);
+		topPane.setPadding(new Insets(10));
+
+		return topPane;
+	}
+
+	/**
+	 * This method will create the leftPane of the root BorderPane layout.
+	 * @return Returns the Node that will be displayed in the leftPane.
+	 */
+    private Node leftPane() {
+        out.println("Adding left pane.");
+
+        root = new TreeItem<>("root");
+        root.setExpanded(true);
+        makeTreeItem("Liquid Feed Assembly", root);
+        makeTreeItem("Coil", root);
+        TreeItem<String> pumps = makeTreeItem("Pumps", root);
+        makeTreeItem("Teikoku", pumps);
+        makeTreeItem("Cornell", pumps);
+
+	    TreeView<String> hierarchyTree = new TreeView<>(root);
+        hierarchyTree.setShowRoot(false);
+
+	    return hierarchyTree;
+    }
+
+	/**
+	 * This method will create the bottomPane of the root BorderPane layout.
+	 * @return Returns the Node that will be displayed in the bottomPane.
+	 */
+	private Node bottomPane() {
+		//create control buttons
+		Button buttonOK = new Button("OK");
+		Button buttonCancel = new Button("Cancel");
+
+		//create radio button drop down
+		RadioButton rbOne = new RadioButton("One");
+		RadioButton rbTwo = new RadioButton("Two");
+		RadioButton rbThree = new RadioButton("Three");
+		ToggleGroup group = new ToggleGroup();
+		group.getToggles().addAll(rbOne, rbTwo, rbThree);
+
+		VBox selectionBox = new VBox(10);
+		selectionBox.setPadding(new Insets(10));
+		selectionBox.getChildren().addAll(rbOne, rbTwo, rbThree);
+
+		TitledPane dropDownBox = new TitledPane("Numbers", selectionBox);
+		dropDownBox.setCollapsible(true);
+
+		//create bottom pane
+		HBox bottomPane = new HBox(10, dropDownBox, spacer, buttonOK, buttonCancel);
+		bottomPane.setPadding(new Insets(10));
+
+		return bottomPane;
+	}
+
+    /**
+     * This class will construct new items for a TreeView.
+     * @param title Title of the new node/leaf item.
+     * @param parent Parent of the new node/leaf item.
+     * @return Returns the new TreeItem created in this method. Returned object can be used to create sub-nodes.
+     */
+    private TreeItem<String> makeTreeItem(String title, TreeItem<String> parent) {
+        TreeItem<String> newItem = new TreeItem<>(title);
+        newItem.setExpanded(true);
+        parent.getChildren().add(newItem);
+        return newItem;
     }
 
 }
